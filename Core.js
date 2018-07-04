31965d1b8cdad7c705d88cc43723745acd7570c0
Luke <lr98lr@aol.com>
2018-07-04 19:43:59 -0400

    grey scale noise

--- Core.js
+++ Core.js
@@ -1,5 +1,5 @@
 
-
+var incrementer = 0.01;
 function setup(){
 	createCanvas(720, 480);
 
@@ -7,16 +7,20 @@ function setup(){
 
 
 function draw(){
+	loadPixels();
 
-	background(50);
-	stroke(255);
-	noFill();
-	beginShape();
-	var xoff = 0;
 	for(let x = 0; x < width; x++){
-		stroke(255);
-		vertex(x, noise(xoff+=0.01) * height);
+		for(let y = 0; y < height; y++){
+			let i = (x + y * width) * 4;
+			let r = random(255);
+			pixels[i] = r;
+			pixels[i+1] = r;
+			pixels[i+2] = r;
+			pixels[i+3] = 255;
+		}
 	}
-	endShape();
+
+
+	updatePixels();
 
 }
