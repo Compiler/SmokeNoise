11860f56838653a188ea83869a52dd84450e9a3b
Luke <lr98lr@aol.com>
2018-07-04 19:32:05 -0400

    random 2d

--- Core.js
+++ Core.js
@@ -1,6 +1,22 @@
 
 
 function setup(){
+	createCanvas(720, 480);
 
+}
+
+
+function draw(){
+
+	background(50);
+	stroke(255);
+	noFill();
+	beginShape();
+	var xoff = 0;
+	for(let x = 0; x < width; x++){
+		stroke(255);
+		vertex(x, noise(xoff+=0.01) * height);
+	}
+	endShape();
 
 }
