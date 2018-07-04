e8f53698e350f8affa419fda316a9d03077413c7
Luke <lr98lr@aol.com>
2018-07-04 19:47:54 -0400

    NOISEEEE

--- Core.js
+++ Core.js
@@ -7,17 +7,23 @@ function setup(){
 
 
 function draw(){
+
+
+	let yoff = 0;
 	loadPixels();
 
 	for(let x = 0; x < width; x++){
+		let xoff = 0;
 		for(let y = 0; y < height; y++){
 			let i = (x + y * width) * 4;
-			let r = random(255);
+			let r = noise(xoff, yoff) * 255;
 			pixels[i] = r;
 			pixels[i+1] = r;
 			pixels[i+2] = r;
 			pixels[i+3] = 255;
+			xoff+= 0.01;
 		}
+		yoff+= 0.01;
 	}
 
 
