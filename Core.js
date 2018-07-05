e5c62a33affb462c74546f0e17000d3804350fd8
Luke <lr98lr@aol.com>
2018-07-04 20:02:10 -0400

    setting up flow field

--- Core.js
+++ Core.js
@@ -1,8 +1,18 @@
 
-var incrementer = 0.01;
+var incrementer = 0.1;
+
+let scale = 20;
+let cols, rows;
+
+
+let frames;
+
 function setup(){
 	createCanvas(720, 480);
+	cols = floor(width / scale);
+	rows = floor(height / scale);
 
+	frames = createP('');
 }
 
 
@@ -10,23 +20,23 @@ function draw(){
 
 
 	let yoff = 0;
-	loadPixels();
-
-	for(let x = 0; x < width; x++){
+	for(let y = 0; y < rows; y++){
 		let xoff = 0;
-		for(let y = 0; y < height; y++){
+		for(let x = 0; x < cols; x++){
+
 			let i = (x + y * width) * 4;
 			let r = noise(xoff, yoff) * 255;
-			pixels[i] = r;
-			pixels[i+1] = r;
-			pixels[i+2] = r;
-			pixels[i+3] = 255;
+
+			fill(r);
+			rect(x * scale, y * scale, scale, scale);
+
+
 			xoff+= 0.01;
 		}
 		yoff+= 0.01;
 	}
 
+	frames.html(floor(frameRate()));
 
-	updatePixels();
 
 }
